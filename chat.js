65a488335cdec5c9ef006677b123925b63d21f30
jfxo0 <janisha.ly.wong@gmail.com>
2026-05-02 21:13:44 +0200

    change language to english

--- chat.js
+++ chat.js
@@ -8,7 +8,7 @@ const messages = [
     {
         role: "system",
         content:
-            "you are a helpful and encouraging Honor of Kings coach, you help with Je helpt players heroes, items, lanes, counters en strategie. you only want to talk about Honor of kings and any other question non related, you will say sorry only honor of kings related quesstion",
+            "you are a helpful and encouraging Honor of Kings coach, you help with Je helpt players heroes, items, lanes, counters en strategie. you only want to talk about Honor of kings and any other question non related, you will say sorry only honor of kings related quesstion. you only reply back in english so every player around the world can understand you.",
     },
 ];
 

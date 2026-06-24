b0486b1876c386530ef6fd0151d21038f926db2d
jfxo0 <janisha.ly.wong@gmail.com>
2026-06-24 16:25:20 +0200

    added the keys

--- chat.js
+++ chat.js
@@ -2,6 +2,10 @@ import { AzureChatOpenAI } from "@langchain/openai";
 
 const model = new AzureChatOpenAI({
     temperature: 0.4,
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+    azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
 });
 
 const messages = [
